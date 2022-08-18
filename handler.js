44aea4360d5bade5f99dfd94ac972e54bf2108c3
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-08-18 16:34:47 -0400

    Add send to Discord webhook

--- handler.js
+++ handler.js
@@ -1,16 +1,62 @@
+'use strict';
+
 require("dotenv").config();
 const { Client } = require("twitter-api-sdk");
-const contentMatch = /nerf(s)?|buff(s)?|change(s)?/gmi;
+const { EmbedBuilder, WebhookClient } = require("discord.js");
+
+const contentMatch = /nerf(s)?|buff(s)?|change(s)?:?\n/gmi;
 const contentNotMatch = /skin(s)?|model(s)?|chroma(s)?/gmi;
-const user1 = "3855898996"
+const twtUser = "3855898996";
+let lastTweetId;
+
+const createEmbed = (tweetData, userData) => {
+  // Destructure variables
+  const { text, id, created_at } = tweetData;
+  const { username, profile_image_url, profile_url, tweets_url } = userData;
+  const [champ, changes] = text.split(':');
+
+  // Create embed from tweet data
+  return new EmbedBuilder()
+    .setColor('#28c700')
+    .setTitle(champ)
+    .setURL(tweets_url + id)
+    .setDescription(changes.trim().replaceAll('*', '\u2022'))
+    .setAuthor({
+      name: username,
+      url: profile_url,
+      iconURL: profile_image_url
+    })
+    .setFooter({
+      text: 'Tweeted at ' + require("dayjs")(created_at).format('h:mm A'),
+      iconURL: 'https://i0.wp.com/www.apacph.org/wp/wp-content/uploads/2014/03/Twitter-Logo-New-.png?fit=518%2C518&ssl=1'
+    })
+}
 
 exports.handler = async (event) => {
+  // Instantiate Twitter client
+  const webhookClient = new WebhookClient({ url: process.env.DISCORD_WEBHOOK })
   const client = new Client(process.env.TWITTER_BEARER_TOKEN);
-  const response = await client.tweets.usersIdTweets(user1, {
-    "since_id": undefined,
+
+  // Get user data
+  const user = await client.users.findUserById(twtUser, {
+    "user.fields": [
+      "profile_image_url",
+      "username"
+    ]
+  });
+  const userData = {
+    ...user.data,
+    profile_url: `https://twitter.com/${user.data.username}`,
+    tweets_url: `https://twitter.com/${user.data.username}/status/`
+  }
+
+  // Get recent tweets
+  const tweets = await client.tweets.usersIdTweets(twtUser, {
+    "since_id": lastTweetId,
     "max_results": 100,
     "tweet.fields": [
-      "text"
+      "text",
+      "created_at"
     ],
     "expansions": [
       "in_reply_to_user_id",
@@ -18,13 +64,26 @@ exports.handler = async (event) => {
     ],
   });
 
-  const { data } = response;
-  const primaryFilter = data.filter(({ text, in_reply_to_user_id }) =>
+  // Update last posted tweet
+  lastTweetId = tweets.data[0].id;
+
+  // Filter recent tweets for relevant content
+  const tweetsData = Array.from(tweets.data).reverse();
+  const primaryFilter = tweetsData.filter(({ text, in_reply_to_user_id }) =>
     !in_reply_to_user_id && contentMatch.test(text) && !contentNotMatch.test(text))
-  const secondaryFilter = data.filter(t => {
+  const secondaryFilter = tweetsData.filter(t => {
     const refs = t.referenced_tweets?.map(({ id }) => id);
     return primaryFilter.includes(t) || primaryFilter.find(({ id }) => refs?.includes(id));
   });
 
-  console.log("response", JSON.stringify(secondaryFilter, null, 2));
+  for await (const tweet of secondaryFilter) {
+    try {
+      await webhookClient.send({
+        embeds: [createEmbed(tweet, userData)]
+      })
+    } catch (err) {
+      console.error(err);
+    }
+  };
 }
+
