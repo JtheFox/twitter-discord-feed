39c3126b417d2658e3e4ddcaa06c909fbd8aa4e7
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-08-18 22:25:17 -0400

    Update last tweet storage method

--- index.js
+++ index.js
@@ -3,9 +3,32 @@
 require("dotenv").config();
 const { Client } = require("twitter-api-sdk");
 const { EmbedBuilder, WebhookClient } = require("discord.js");
+const dayjs = require("dayjs");
 
-// Global lambda variable
-let lastTweetId;
+// Function to create Discord embeds
+const createEmbed = (tweetData, userData) => {
+  // Destructure variables
+  const { text, id, created_at } = tweetData;
+  const { username, profile_image_url, profile_url, tweets_url } = userData;
+  const [champ, changes] = text.split(':');
+  const changelist = changes.trim().replaceAll('*', '\u2022')
+
+  // Create embed from tweet data
+  return new EmbedBuilder()
+    .setColor('#c1d260')
+    .setTitle(champ)
+    .setURL(tweets_url + id)
+    .setDescription(changelist)
+    .setAuthor({
+      name: username,
+      url: profile_url,
+      iconURL: profile_image_url
+    })
+    .setFooter({
+      text: 'Tweeted at ' + dayjs(created_at).format('h:mm A'),
+      iconURL: 'https://i0.wp.com/www.apacph.org/wp/wp-content/uploads/2014/03/Twitter-Logo-New-.png?fit=518%2C518&ssl=1'
+    })
+}
 
 exports.handler = async (event) => {
   // Tweet filters
@@ -15,6 +38,10 @@ exports.handler = async (event) => {
   // Instantiate Twitter client
   const webhookClient = new WebhookClient({ url: process.env.DISCORD_WEBHOOK_URL })
   const client = new Client(process.env.TWITTER_BEARER_TOKEN);
+
+  // Get last tweet id
+  const response = await fetch(process.env.STORED_JSON_URL);
+  let lastTweet = await response.json();
 
   // Get user data
   const userId = process.env.TWITTER_USER_ID;
@@ -32,7 +59,7 @@ exports.handler = async (event) => {
 
   // Get recent tweets
   const tweets = await client.tweets.usersIdTweets(userId, {
-    "since_id": lastTweetId,
+    "since_id": response.id,
     "max_results": 100,
     "tweet.fields": [
       "text",
@@ -45,7 +72,8 @@ exports.handler = async (event) => {
   });
 
   // Update last pulled tweet index for next query
-  lastTweetId = tweets.data[0].id;
+  lastTweet = { "id": tweets.data[0].id }
+  await fetch(process.env.STORED_JSON_URL, { method: 'POST', body: lastTweet });
 
   // Filter recent tweets for relevant content
   const tweetsData = Array.from(tweets.data).reverse();
@@ -57,31 +85,6 @@ exports.handler = async (event) => {
     const refs = t.referenced_tweets?.map(({ id }) => id);
     return primaryFilter.includes(t) || primaryFilter.find(({ id }) => refs?.includes(id));
   });
-
-  // Function to create Discord embeds
-  const createEmbed = (tweetData, userData) => {
-    // Destructure variables
-    const { text, id, created_at } = tweetData;
-    const { username, profile_image_url, profile_url, tweets_url } = userData;
-    const [champ, changes] = text.split(':');
-    const changelist = changes.trim().replaceAll('*', '\u2022')
-
-    // Create embed from tweet data
-    return new EmbedBuilder()
-      .setColor('#c1d260')
-      .setTitle(champ)
-      .setURL(tweets_url + id)
-      .setDescription(changelist)
-      .setAuthor({
-        name: username,
-        url: profile_url,
-        iconURL: profile_image_url
-      })
-      .setFooter({
-        text: 'Tweeted at ' + require("dayjs")(created_at).format('h:mm A'),
-        iconURL: 'https://i0.wp.com/www.apacph.org/wp/wp-content/uploads/2014/03/Twitter-Logo-New-.png?fit=518%2C518&ssl=1'
-      })
-  }
 
   // Send to Discord webhook
   for await (const tweet of secondaryFilter) {
